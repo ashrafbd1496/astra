aa905aa87395bdc4b890941f77ef3c6630cd7108
Simran <simranc@bsf.io>
2021-10-26 14:36:58 +0530

    Test case for primary footer border size and color

---
+++ tests/e2e/specs/customizer/footer-builder/primary-footer/border-size-color.test.js
@@ -0,0 +1,30 @@
+import { createURL } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
+import { scrollToElement } from '../../../../utils/scroll-to-element';
+describe( 'Below footer border size setting in customizer', () => {
+	it( 'top border size should apply correctly', async () => {
+		const borderSizeColor = {
+			'hb-footer-main-sep': '50',
+			'hb-footer-main-sep-color': 'rgb(255, 79, 88)',
+		};
+		await setCustomize( borderSizeColor );
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]' );
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
+			property: 'border-top-width',
+		} ).cssValueToBe( `${ borderSizeColor[ 'hb-footer-main-sep' ] + 'px' }`,
+		);
+		await expect( {
+			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
+			property: 'border-top-color',
+		} ).cssValueToBe( `${ borderSizeColor[ 'hb-footer-main-sep-color' ] }`,
+
+		);
+	} );
+} );
