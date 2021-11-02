282267bc77ffc4a8c3530020234da2463fb87e01
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2021-11-02 19:07:46 +0530

    Update margin.test.js

--- tests/e2e/specs/customizer/footer-builder/above-footer/margin.test.js
+++ tests/e2e/specs/customizer/footer-builder/above-footer/margin.test.js
@@ -10,7 +10,7 @@ describe( 'Above footer margin setting in customizer', () => {
 					top: '50',
 					right: '50',
 					bottom: '50',
-					left: '50'
+					left: '50',
 				},
 				tablet: {
 					top: '50',
