19c614c7d4bb351b5a7f29013e30fd9f52378f21
Simran <simranc@bsf.io>
2021-11-19 13:49:46 +0530

    test case for footer builder social icon size

---
+++ tests/e2e/specs/customizer/footer-builder/social-icon/icon-size.test.js
@@ -0,0 +1,103 @@
+import { createURL, setBrowserViewport } from '@wordpress/e2e-test-utils';
+import { setCustomize } from '../../../../utils/customize';
+import { scrollToElement } from '../../../../utils/scroll-to-element';
+describe( 'Social Icons in the customizer', () => {
+	it( 'footer social icon size for desktop should apply correctly', async () => {
+		const socialiconSize = {
+			'footer-social-1-size': {
+				desktop: '50px',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'social-icons-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( socialiconSize );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg' );
+		await setBrowserViewport( 'large' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg',
+			property: 'width',
+		} ).cssValueToBe( `${ socialiconSize[ 'footer-social-1-size' ].desktop }` );
+
+		await expect( {
+			selector: '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg',
+			property: 'height',
+		} ).cssValueToBe( `${ socialiconSize[ 'footer-social-1-size' ].desktop }` );
+	} );
+
+	// it( 'footer social icon size for tablet should apply correctly', async () => {
+	// 	const socialiconSize = {
+	// 		'footer-mobile-1-size': {
+	// 			tablet: '30',
+	// 		},
+	// 		'footer-desktop-items': {
+	// 			primary: {
+	// 				primary_2: {
+	// 					0: 'social-icons-1',
+
+	// 				},
+	// 			},
+	// 		},
+	// 	};
+	// 	await setCustomize( socialiconSize );
+
+	// 	await page.goto( createURL( '/' ), {
+	// 		waitUntil: 'networkidle0',
+	// 	} );
+	// 	await page.waitForSelector( '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg' );
+	// 	await setBrowserViewport( 'medium' );
+	// 	await scrollToElement( '#colophon' );
+	// 	await expect( {
+	// 		selector: '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg',
+	// 		property: 'width',
+	// 	} ).cssValueToBe( `${ socialiconSize[ 'footer-social-1-size' ].tablet }` );
+
+	// 	await expect( {
+	// 		selector: '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg',
+	// 		property: 'height',
+	// 	} ).cssValueToBe( `${ socialiconSize[ 'footer-social-1-size' ].tablet }` );
+	// } );
+
+	it( 'footer social icon size for mobile should apply correctly', async () => {
+		const socialiconSize = {
+			'footer-social-1-size': {
+				mobile: '17px',
+			},
+			'footer-desktop-items': {
+				primary: {
+					primary_2: {
+						0: 'social-icons-1',
+
+					},
+				},
+			},
+		};
+		await setCustomize( socialiconSize );
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+		await page.waitForSelector( '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg' );
+		await setBrowserViewport( 'small' );
+		await scrollToElement( '#colophon' );
+		await expect( {
+			selector: '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg',
+			property: 'width',
+		} ).cssValueToBe( `${ socialiconSize[ 'footer-social-1-size' ].mobile }` );
+
+		await expect( {
+			selector: '.ast-footer-social-1-wrap .footer-social-inner-wrap .ast-builder-social-element svg',
+			property: 'height',
+		} ).cssValueToBe( `${ socialiconSize[ 'footer-social-1-size' ].mobile }` );
+	} );
+} );
